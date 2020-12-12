d1e5e07c3def4ab2fb792e6edb8a84c5108f670a
TakumiImamura <72206661+TakumiImamura@users.noreply.github.com>
2020-12-12 13:23:50 +0900

    Update ByteOrder.c

--- ByteOrder.c
+++ ByteOrder.c
@@ -2,6 +2,8 @@
 int main(void){
   int i = 0x01234567;
   unsigned char *p = (unsigned char *)&i;
+  if(*p == 0x01) printf("Big endian");
+  else printf("Little endian");
   printf("%x:%x:%x:%x\n", p[0], p[1], p[2], p[3]);
   return 0;
 }
